33001a326dd9c505688a85b3b354487c6bb225a1
Erbeth Charte <echarte@tutanota.com>
2021-09-15 10:31:06 -0400

    hobbit complete.

--- hobbit/src/hobbit/core.clj
+++ hobbit/src/hobbit/core.clj
@@ -7,8 +7,32 @@
                              {:name "nose" :size 1}
                              {:name "neck" :size 2}
                              {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn matching-part
+  "Complete the right characteristics."
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "rigth-")
+   :size (:size part)})
+
+(defn symetrize-body-parts
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
