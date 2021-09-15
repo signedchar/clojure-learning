2867bae98a246616ea10a29c1a8a35a60ec1b24a
Erbeth Charte <echarte@tutanota.com>
2021-09-14 23:11:02 -0400

    skel

---
+++ hobbit/src/hobbit/core.clj
@@ -0,0 +1,14 @@
+(ns hobbit.core)
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
