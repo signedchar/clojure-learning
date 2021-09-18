003d0dc1b7dadaa36ae62349e9be66b0d2dbeb2c
Erbeth Charte <echarte@tutanota.com>
2021-09-17 23:58:57 -0400

    (nw) hit points fn new function for hitpoints.

--- hobbit/src/hobbit/core.clj
+++ hobbit/src/hobbit/core.clj
@@ -44,3 +44,14 @@
                   (set [part (matching-part part)])))
           []
           asym-body-parts))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-num)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
