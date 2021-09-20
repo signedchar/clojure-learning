2e5b96ea2556209ca6ed47e924c4ef5b190f2dbc
Erbeth Charte <echarte@tutanota.com>
2021-09-19 21:29:03 -0400

    Testing the main function for sum the multiples of 3 or 5 from a list.

--- mult_3_5/test/mult_3_5/core_test.clj
+++ mult_3_5/test/mult_3_5/core_test.clj
@@ -3,5 +3,5 @@
             [mult-3-5.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Summatory divisible numbers by 3 or 5 from 0 to 9."
+    (is (= 23 (mult-3-5 10)))))
