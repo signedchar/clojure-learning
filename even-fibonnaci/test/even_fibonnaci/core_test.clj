f4fa5c1f3b78556c7bc3d5539833393e1a86925a
Erbeth Charte <echarte@tutanota.com>
2021-09-21 23:37:15 -0400

    doc and test. More descriptive documentation Adding test.

--- even-fibonnaci/test/even_fibonnaci/core_test.clj
+++ even-fibonnaci/test/even_fibonnaci/core_test.clj
@@ -3,5 +3,5 @@
             [even-fibonnaci.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Sum even numbers of fibonacci."
+    (is (= 44 (fib-even 10)))))
